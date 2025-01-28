a0d9640b992dfb79656c4b0a8ada0ef9bcf35130
Umar Solanki <uysolanki@gmail.com>
2025-01-28 16:37:47 +0530

    FE day 1

--- src/main/java/com/itvedant/cms/controller/CustomerControllerFE.java
+++ src/main/java/com/itvedant/cms/controller/CustomerControllerFE.java
@@ -1,15 +1,22 @@
 package com.itvedant.cms.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itvedant.cms.entity.Customer;
+import com.itvedant.cms.service.CustomerService;
 
 @Controller
 @RequestMapping("/cms/v1")
 public class CustomerControllerFE {
+	
+	@Autowired
+	CustomerService customerService;
 	
 	@GetMapping("/welcomefe")
 	public String welcome(Model model)
@@ -23,6 +30,14 @@ public class CustomerControllerFE {
 		
 		model.addAttribute("c1",customer);
 		return "welcome";
+	}
+	
+	@GetMapping("/displayAllCustomers")
+	public String displayAllCustomers(Model model)
+	{
+		List<Customer> customers=customerService.getAllCustomers();
+		model.addAttribute("customers",customers);
+		return "customer-list";
 	}
 
 }
